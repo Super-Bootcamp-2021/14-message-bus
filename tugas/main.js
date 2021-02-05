0f79eb63fd5440838c1bcd2ba00b8e0121f0c542
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-02-05 15:35:57 +0700

    menambahkan perintah untuk menjalankan "object-storage" service dan "database-storage" service

--- tugas/main.js
+++ tugas/main.js
@@ -2,17 +2,21 @@ const server = require('./task/server');
 
 function main(argv) {
   switch (argv[2]) {
-    case 'basic':
-      require('./storage-service/main');
+    case 'database':
+      require('./database-storage/main');
       break;
     case 'task':
       server.run();
       break;
+    case 'object':
+        require('./object-storage/main');
+        break;
     default:
       console.log(argv[2]);
       process.stdout.write('available commmand are \n');
-      process.stdout.write('- basic\n');
+      process.stdout.write('- database\n');
       process.stdout.write('- task\n');
+      process.stdout.write('- object\n');
       process.stdout.write(':D\n');
   }
 }
