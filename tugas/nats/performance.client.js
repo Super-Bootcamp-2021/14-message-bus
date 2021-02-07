23df6c4615f0acb8e28e9e6c5ddc218c6a70fe78
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-02-07 21:34:33 +0700

    membuat nats command "worker.created" dan function workerLog() untuk mengirim request ke performance-service

--- tugas/nats/performance.client.js
+++ tugas/nats/performance.client.js
@@ -1,3 +1,4 @@
+const { worker } = require('cluster');
 const http = require('http');
 
 const PORT = 3232;
@@ -23,6 +24,28 @@ function taskLog(data) {
   });
 }
 
+function workerLog(data) {
+  return new Promise((resolve, reject) => {
+    const req = http.request(
+      `http://localhost:${PORT}/worker?data=${data}`,
+      (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+          data += chunk.toString();
+        });
+        res.on('end', () => {
+          resolve(data);
+        });
+        res.on('error', (err) => {
+          reject(err);
+        });
+      }
+    );
+    req.end();
+  });
+}
+
 module.exports = {
   taskLog,
+  workerLog
 };
